4e3c1f2dc6dd1a3dc34255273c089a81ea4f97a3
Alex Spina <alexfspina@gmail.com>
2024-09-03 23:48:37 +0100

    initial documentation

---
+++ R/AJS_chad_descriptors.R
@@ -0,0 +1,28 @@
+#' @title Data from an outbreak of Acute Jaundice Syndrome (AJS) in Chad 2016
+#'
+#' @description
+#' Data from a real outbreak of Hepatitis E virus (HEV) infection which
+#' occurred in the town of Am Timan, Chad, between October 2016 and
+#' April 2017. 
+#'  
+#' @details
+#' Please note, that some data has been adapted in order to best achieve
+#' training objectives. Also, the GPS coordinates included in the dataset
+#' do not correspond to real cases identified during this outbreak.
+#' They have been generated exclusively for training purposes.
+#'  
+#' @source This data was collected as part of Doctors Without Borders (MSF) outbreak 
+#' response and the the Ministry of Health (MoH) in Chad has approved the use 
+#' of this data for training purposes.
+#' This dataset is licensed under [GPL3](https://www.gnu.org/licenses/gpl-3.0.html) 
+#' 
+#' @references {Spina A, Lenglet A, et al. (2017) A large outbreak of Hepatitis E virus genotype 1 infection in an urban setting in Chad likely linked to household level transmission factors, 2016-2017. PLoS ONE 12(11): e0188240} \doi{10.1371/journal.pone.0188240}
+#' @format A data frame with 1477 rows and 122 columns:
+#' \describe{
+#'   \item{blabla}{explanation of a variable}
+#'   \item{blabla}{more explanation of variables}
+#' }
+#' 
+#' @docType data
+#' @name AJS_AmTiman
+NULL
