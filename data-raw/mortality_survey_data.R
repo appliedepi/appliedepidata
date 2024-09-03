a56c0ddb32e2f09d7dbe36af3beac14fe690a211
Alex Spina <alexfspina@gmail.com>
2024-09-03 22:48:29 +0100

    using lists for excel files w multiple sheets

---
+++ data-raw/mortality_survey_data.R
@@ -0,0 +1,18 @@
+## code to prepare `mortality_survey_data` dataset goes here
+# This creates an .rda format of the mortality_survey_data.xlsx file
+
+# Define the path to the Excel file in inst/extdata
+file_path <- system.file("extdata", "mortality_survey_data.xlsx", package = "appliedepidata")
+
+# Read in the Excel file using rio
+# note this reads in all sheets in to a list (for merging household and members)
+mortality_survey_data <- rio::import_list(file_path)
+
+# process data as needed (e.g. cleaning, recoding, restructuring)
+
+# Save the data as an internal .rda file in the data/ directory
+# This will make the dataset internal (not accessible via data()) 
+# it is still available within the package but as package::dataset
+# (the description file has 'LazyData: true' so data now wont take any memory 
+# unless it is actively called)
+usethis::use_data(mortality_survey_data, overwrite = TRUE)
--- data-raw/mortality_survey_dict.R
+++ data-raw/mortality_survey_dict.R
@@ -10,5 +10,11 @@ mortality_survey_dict <- epidict::msf_dict_survey(name = file_path,
                                                   compact = FALSE, 
                                                   long = TRUE)
 
+# process data as needed (e.g. cleaning, recoding, restructuring)
 
+# Save the data as an internal .rda file in the data/ directory
+# This will make the dataset internal (not accessible via data()) 
+# it is still available within the package but as package::dataset
+# (the description file has 'LazyData: true' so data now wont take any memory 
+# unless it is actively called)
 usethis::use_data(mortality_survey_dict, overwrite = TRUE)
