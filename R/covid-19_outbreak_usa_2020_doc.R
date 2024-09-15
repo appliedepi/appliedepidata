5791ce796bd80ae4c1c07f743ed2dbffadf5f2af
Alex Spina <alexfspina@gmail.com>
2024-09-15 22:04:52 +0100

    rename doc files to match group name, move rds to extdata, change to eue, devtools::document and rebuild

--- R/covid-19_outbreak_usa_2020_doc.R
+++ R/covid-19_outbreak_usa_2020_doc.R
@@ -14,7 +14,8 @@
 #' do not correspond to real cases identified during this outbreak.
 #' They have been generated exclusively for training purposes.
 #'  
-#' @source This data was collected by Fulton County for surveillance purposes, then anonymized and jittered for training. 
+#' @source This data was collected by Fulton County for surveillance purposes, 
+#' then anonymized and jittered for training. 
 #' 
 #' This is version 1.0.  
 #' 
@@ -33,19 +34,21 @@
 #' @description
 #' Anonymized and jittered linelist data from the real emergence of COVID-19 in 
 #' Fulton County, with sample dates from early 2020 to July 2021. This data has been
-#' cleaned to standardise column names and content, format column classes, and remove erroneous data. 
+#' cleaned to standardise column names and content, format column classes, and 
+#' remove erroneous data. 
 #'  
 #' @details
 #' This is a case-linelist which contains de-indentified individual level data 
 #' on clinical, laboratory and epidemiological information. It is the clean version of 
-#' \link{fulton_data} and can be used together with \link{fulton_population} (population data by county). 
+#' \link{fulton_data} and can be used together with \link{fulton_population}(population data by county). 
 #' 
 #' Please note, that some data has been adapted in order to best achieve
 #' training objectives. Also, the GPS coordinates included in the dataset
 #' do not correspond to real cases identified during this outbreak.
 #' They have been generated exclusively for training purposes.
 #'  
-#' @source This data was collected by Fulton County for surveillance purposes, then anonymized and jittered for training. 
+#' @source This data was collected by Fulton County for surveillance purposes, 
+#' then anonymized and jittered for training. 
 #' 
 #' This is version 2.0.  
 #' 
@@ -57,15 +60,16 @@
 "fulton_data_clean"
 
 
-#' @title Zip population data Fulton County (USA), 2020
+#' @title Zip-code population data Fulton County (USA), 2020
 #'
 #' @description
 #' Table of population size per zip code in Fulton County (USA), 2020
 #'  
 #' @details
-#' This is an aggregate dataset with population size per zip code in Fulton County (USA), 2020.
-#' It can be used together with \link{fulton_data} or \link{fulton_data_clean}, which are raw and
-#' clean versions of COVID-19 linelist data for Fulton County respectively. 
+#' This is an aggregate dataset with population size per zip code in Fulton 
+#' County (USA), 2020. It can be used together with \link{fulton_data} or 
+#' \link{fulton_data_clean}, which are raw and clean versions of COVID-19 
+#' linelist data for Fulton County respectively. 
 #' 
 #' Please note, that some data has been adapted in order to best achieve
 #' training objectives. 
--- data-raw/fulton_data_clean.R
+++ data-raw/fulton_data_clean.R
@@ -1,7 +1,7 @@
 ## code to prepare `fulton_data_clean` dataset goes here
 
 # Define the path to the Excel file in inst/extdata
-file_path <- system.file("data", "fulton_data_clean.rds", package = "appliedepidata")
+file_path <- system.file("extdata", "fulton_data_clean.rds", package = "appliedepidata")
 
 # Read in the Excel file using rio
 fulton_data_clean <- rio::import(file_path)
