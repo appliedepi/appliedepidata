38583f795b72d0d49e726f6db3ddde7fc6c9aa44
Alanah <165188019+alanahjansen@users.noreply.github.com>
2026-07-03 11:17:44 -0700

    Added translated datasets
    Added translated datasets (pt, fr, es) of feveriosis case study

---
+++ data-raw/feveriosis_main_fr.R
@@ -0,0 +1,14 @@
+## code to prepare `feveriosis_main_fr` dataset goes here
+
+# Define the path to the Excel file in inst/extdata
+file_path <- system.file("extdata", "feveriosis_main_fr.xlsx", package = "appliedepidata")
+
+# Read in the Excel file using rio
+feveriosis_main_fr <- rio::import(file_path)
+
+# Other code for editing the file can go here
+
+
+# Save the data as an internal .rda file in the data/ directory
+
+usethis::use_data(feveriosis_main_fr, overwrite = TRUE)
