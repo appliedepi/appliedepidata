038e1042308e251c807c2e7858c40aaa94b8c304
Alanah <165188019+alanahjansen@users.noreply.github.com>
2026-07-09 14:21:36 -0700

    Added ru datasets
    Added Russian datasets for the feveriosis_surveillance case study

---
+++ data-raw/feveriosis_recent_ru.R
@@ -0,0 +1,14 @@
+## code to prepare `feveriosis_recent_ru` dataset goes here
+
+# Define the path to the Excel file in inst/extdata
+file_path <- system.file("extdata", "feveriosis_recent_ru.csv", package = "appliedepidata")
+
+# Read in the Excel file using rio
+feveriosis_recent_ru <- rio::import(file_path)
+
+# Other code for editing the file can go here
+
+
+# Save the data as an internal .rda file in the data/ directory
+
+usethis::use_data(feveriosis_recent_ru, overwrite = TRUE)
